1ac4352742eaf8cc70a271dea8a1d88dfb83f8bf
Alx31 <alex31.couderc@gmail.com>
2019-02-25 22:56:17 +0100

    gzosp

--- sdm845.mk
+++ sdm845.mk
@@ -14,7 +14,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vend
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-validus
+    $(LOCAL_PATH)/overlay-gzosp
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
